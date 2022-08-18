8b5888f6dde134b5a3ecdb45225b6c0820ac5001
leehj050211 <leehj050211@gmail.com>
2022-08-18 16:01:23 +0900

    Initial commit

---
+++ src/main/java/bsm/speaker/SpeakerApplication.java
@@ -0,0 +1,13 @@
+package bsm.speaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakerApplication.class, args);
+	}
+
+}
